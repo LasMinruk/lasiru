ef65b83e82e2e922d513442d83f132be59f5e643
LasMinruk <lasiru2225@gmail.com>
2025-01-27 03:13:05 +0530

    responsive las

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <h1 className="logo">Lasiru Minruk</h1>
-      <ul className="nav-links">
-        <li><Link to="education" smooth={true} duration={800}>Skills & Education</Link></li>
+      <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <Link to="education" smooth={true} duration={800} onClick={toggleMenu}>
+            Skills & Education
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="contact" smooth={true} duration={800} onClick={toggleMenu}>
+            Contact
+          </Link>
+        </li>
       </ul>
     </nav>
   );
